be177558e5e56f81f8c374e85bae83a66bea6bca
Yuri <dr.dampel@yandex.ru>
2022-07-07 19:01:40 +0300

    Work on dreamField

--- src/components/HWDreamsField/DreamsFieldComponent.jsx
+++ src/components/HWDreamsField/DreamsFieldComponent.jsx
@@ -3,13 +3,43 @@ import Word from './WordComponent';
 import s from './DreamField.module.css';
 import photo from './../../img/photoP.png';
 import { useState } from 'react';
+import { useCookies } from 'react-cookie';
+import { logDOM } from '@testing-library/react';
+import { useEffect } from 'react';
 
 export default function DreamsField(){
     const word = "WORKER";
+    const [cookies, setCookies, removeCookies] = useCookies(["WORKER"]);
+    console.log("line 13 " + cookies.word._letters);
     const wordStart = (10 / 2) - (word.length / 2);
 
     const [userSymb, setUserSymb] = useState();
     const [arr, setValue] = useState(Factory(wordStart, word, null));
+
+    useEffect(() =>{
+        //debugger
+        console.log("21 " + cookies.word.word);
+        console.log("22 " + cookies.word._letters);
+        
+        if(cookies.word){
+            let w = {
+                word: cookies.word.word ? cookies.word.word : "WORKER",
+                _letters: cookies.word._letters ? cookies.word._letters : []
+            }
+            setCookies("word", w, {path: "/"});
+        }
+        else{
+            let w = {
+                word: "WORKER",
+                _letters:[]
+            }
+            setCookies("word", w, {path: "/"});
+        }
+
+        return () =>{
+            //console.log(cookies.word._letters);
+        };
+    })
 
     const handlerEnterSymb = (e) => {
         if(e.nativeEvent.data.length == 1){
@@ -19,8 +49,13 @@ export default function DreamsField(){
 
     const handlerSayLetter = (e) =>{
         if(word.includes(userSymb)){
-            setValue(Check(arr, userSymb));
+            setValue(Check(arr, userSymb, save));
         }
+    }
+
+    const save = (_letter) => {
+        cookies.word._letters.push(_letter);
+        //console.log(cookies.word._letters);
     }
 
     return(
@@ -62,12 +97,13 @@ function Factory(_start, _word, _letter){
     return arr;
 }
 
-function Check(_arr, _letter){
+function Check(_arr, _letter, cb){
     const newArr = [];
     for (let i = 0; i < _arr.length; i++) {
 
         if(_letter != null && _arr[i].props.Symb == _letter){
             newArr.push(<Word key={i} Symb={_letter} IsVisible={true}/>);
+            cb(_letter);
         }
         else{
             newArr.push(_arr[i]);
@@ -75,4 +111,11 @@ function Check(_arr, _letter){
     }
 
     return newArr;
+}
+
+function CheclWin(_arr, _word){
+    const isWin = false;
+    for (let i = 0; i < _arr.length; i++) {
+        
+    }
 }
--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { CookiesProvider } from 'react-cookie';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App/>
-    </BrowserRouter>    
-  </React.StrictMode>
+  <CookiesProvider>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App/>
+      </BrowserRouter>
+    </React.StrictMode>
+  </CookiesProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
